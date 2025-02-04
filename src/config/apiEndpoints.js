6d6a7f5078ad5ce2acef8fa3a0dcfb9fd0208e96
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-04 11:02:55 -0500

    fix: :bug: change ip server

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -3,7 +3,7 @@
 const API_ENDPOINTS = {
     PRODUCTS: process.env.NEXT_PUBLIC_PRODUCTS_API,
     CATEGORIES: process.env.NEXT_PUBLIC_CATEGORIES_API,
-    AUTH: "http://52.55.97.136:5000",
+    AUTH: "http://54.197.16.41:5000",
     ORDERS: process.env.NEXT_PUBLIC_ORDERS_API,
   };
   
