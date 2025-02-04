648028f69c1b6ed2d5523ec4896ea4b47db3beb9
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-04 09:18:09 -0500

    fix: :bug: without image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,27 +1,16 @@
 export default function ProductCard({ product }) {
     return (
-        <div className="bg-white rounded-lg shadow-md p-4">
-            {/* Image product */}
-            <img
-        src={product.image_url} // Usa la URL de la imagen
-        alt={product.name}
-        className="w-full h-48 object-cover rounded-md mb-4"
-      />
-            {/* Name product */}
-            <h3 className="text-lg font-semibold">{product.name}</h3>
-            {/* Description product */}
-            <p className="text-gray-600 text-sm mb-2">{product.description}</p>
-            {/* Price */}
-            <p className="text-gray-800 font-bold">${parseFloat(product.price).toFixed(2)}</p>
-            {/* Buttons */}
-            <div className="mt-4 flex justify-between">
-                <button className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">
-                    See Details
-                </button>
-                <button className="bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700">
-                    Add Cart
-                </button>
-            </div>
+      <div className="bg-white rounded-lg shadow-md p-4">
+        <h3 className="text-lg font-semibold">{product.name}</h3>
+        <p className="text-gray-600">${parseFloat(product.price).toFixed(2)}</p>
+        <div className="mt-4 flex justify-between">
+          <button className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">
+            Ver detalles
+          </button>
+          <button className="bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700">
+            Agregar al carrito
+          </button>
         </div>
+      </div>
     );
-}
+  }
