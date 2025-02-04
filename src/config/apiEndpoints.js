20a6657b5fe68f404033cdb7d9446c41389d93e1
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-04 17:13:50 -0500

    feat: :fire: change ips

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -1,9 +1,9 @@
 // src/config/apiEndpoints.js
 
 const API_ENDPOINTS = {
-    PRODUCTS: "http://54.172.7.9:5000/productos",
+    PRODUCTS: "http://3.83.124.154:5000/productos",
     CATEGORIES: process.env.NEXT_PUBLIC_CATEGORIES_API,
-    AUTH: "http://54.197.16.41:5000",
+    AUTH: "http://18.207.189.100:5000",
     ORDERS: process.env.NEXT_PUBLIC_ORDERS_API,
   };
   
