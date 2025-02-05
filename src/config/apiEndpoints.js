9b205fd13227e6b62e71f690f5cf2411945a2fcd
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-05 18:16:45 -0500

    fix: :bug: change ip

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -1,9 +1,9 @@
 // src/config/apiEndpoints.js
 
 const API_ENDPOINTS = {
-    PRODUCTS: "http://44.205.251.37:5000/productos",
+    PRODUCTS: "http://3.93.82.110:5000/productos",
     CATEGORIES: process.env.NEXT_PUBLIC_CATEGORIES_API,
-    AUTH: "http://18.208.138.37:5000",
+    AUTH: "http://44.203.138.227:5000",
     ORDERS: process.env.NEXT_PUBLIC_ORDERS_API,
   };
   
