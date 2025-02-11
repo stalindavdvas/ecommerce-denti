acf440437338d93d0b8cc596d986bb86cea5545c
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-11 11:29:01 -0500

    fix: :zap: update ips

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -1,9 +1,9 @@
 // src/config/apiEndpoints.js
 
 const API_ENDPOINTS = {
-    PRODUCTS: "http://3.93.82.110:5000/productos",
+    PRODUCTS: "http://52.70.251.25:5000/productos",
     CATEGORIES: process.env.NEXT_PUBLIC_CATEGORIES_API,
-    AUTH: "http://44.203.138.227:5000",
+    AUTH: "http://44.202.134.28:5000",
     ORDERS: process.env.NEXT_PUBLIC_ORDERS_API,
   };
   
