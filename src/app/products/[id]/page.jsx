b8bde6c5bb150fbeb378ac39438aad66ba7c230d
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-11 13:53:34 -0500

    fix: :bug: test

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -54,7 +54,7 @@ export default function ProductDetail({ params }) {
           </p>
           {/* Botón de agregar al carrito */}
           <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">
-           Add to cart
+           Add to Cart
           </button>
         </div>
       </div>
