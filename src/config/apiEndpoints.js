daae554541bc9436183e6e28ff1ad1ec69a50b20
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-12 18:44:56 -0500

    fix: :bug: change ips

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -4,6 +4,8 @@ const API_ENDPOINTS = {
     PRODUCTS: "http://35.175.137.190:5000/productos",
     PRODUCTSID: "http://34.235.89.199:5000/products",
     ADDCART: "http://3.80.216.223:8080/api/addcart",
+    GETCOUNTCART: "http://98.84.101.7:8080/api/cart/count",
+    GETCARTPRODUCTS: "http://3.80.216.223:8080/api/addcart",
     CATEGORIES: process.env.NEXT_PUBLIC_CATEGORIES_API,
     AUTH: "http://44.210.146.112:5000",
     ORDERS: process.env.NEXT_PUBLIC_ORDERS_API,
