7ca817c88931d2c8a946a89ed2866fb377bda819
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-13 07:34:31 -0500

    fix: :bug: change ip

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -1,13 +1,15 @@
 // src/config/apiEndpoints.js
 
 const API_ENDPOINTS = {
-    PRODUCTS: "http://3.89.195.254:5000/productos",
-    PRODUCTSID: "http://54.90.201.218:5000/products",
-    ADDCART: "http://54.147.172.4:8080/api/addcart",
-    GETCART: "http://174.129.74.211:4567/api/getcart",
-    GETCOUNTCART: "http://54.88.186.40:8080/api/cart/count",
+    PRODUCTS: "http://3.83.184.13:5000/productos",
+    PRODUCTSID: "http://54.163.59.78:5000/products",
+    ADDCART: "http://52.23.205.199:8080/api/addcart",
+    GETCART: "http://3.87.215.1:4567/api/getcart",
+    GETCOUNTCART: "http://54.88.249.69:8080/api/cart/count",
+    EDITCART: "http://3.84.40.86:4567/api/cart",
+    DELETECART: "http://3.84.40.86:4567/api/cart",
     CATEGORIES: process.env.NEXT_PUBLIC_CATEGORIES_API,
-    AUTH: "http://54.224.180.150:5000",
+    AUTH: "http://54.164.68.236:5000",
     ORDERS: process.env.NEXT_PUBLIC_ORDERS_API,
   };
   
