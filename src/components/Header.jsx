03a4f764bdf0d52a17c7e4106de1dc2561a1c371
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-12 19:37:58 -0500

    fix: :bug: route cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -55,7 +55,7 @@ export default function Header() {
             </>
           )}
           {/* Agregar el carrito */}
-          <Link href="/cart" className="relative hover:underline">
+          <Link href="../app/cart" className="relative hover:underline">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
