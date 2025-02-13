93d2ffe9650ac8d1a20bea8c2c7b1f805d39b334
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-13 01:14:54 -0500

    fix: :bug: new with ruby

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -4,7 +4,7 @@ const API_ENDPOINTS = {
     PRODUCTS: "http://3.89.195.254:5000/productos",
     PRODUCTSID: "http://54.90.201.218:5000/products",
     ADDCART: "http://54.147.172.4:8080/api/addcart",
-    GETCART: "http://44.223.20.37:8080/api/cart",
+    GETCART: "http://174.129.74.211:4567/api/getcart",
     GETCOUNTCART: "http://54.88.186.40:8080/api/cart/count",
     CATEGORIES: process.env.NEXT_PUBLIC_CATEGORIES_API,
     AUTH: "http://54.224.180.150:5000",
